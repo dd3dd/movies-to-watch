6ffecd509a722b018e082377854bd5a546664b01
dd3dd <jacky81810@gmail.com>
2024-09-28 23:37:07 +0800

    flx/update state in local

--- app/ui/like-button.tsx
+++ app/ui/like-button.tsx
@@ -5,28 +5,34 @@ import { addFavoriteMovie, deleteFavoriteMovie } from "../lib/db";
 import { useUserContext } from "../user-provider";
 import { Movie } from "../lib/definitions";
 import { favoriteExist } from "../lib/utils";
-import { fetchFavoriteMovies } from "../lib/db";
+import Spinner from "./spinner";
+// import { fetchFavoriteMovies } from "../lib/db";
 
 export default function LikeButton({ movie }: { movie: Movie }) {
   const { id } = movie;
-  const { user, favoriteMovies, setFavoriteMovies } = useUserContext();
+  const { user, favoriteMovies, setFavoriteMovies, favoriteMoviesLoading } =
+    useUserContext();
+  if (favoriteMoviesLoading) return <Spinner />;
   const exists = favoriteExist(id, favoriteMovies);
 
-  const fetchMovies = async () => {
-    const movies = await fetchFavoriteMovies(user?.email ?? "");
-    setFavoriteMovies(movies || []);
-  };
+  // const fetchMovies = async () => {
+  //   const movies = await fetchFavoriteMovies(user?.email ?? "");
+  //   setFavoriteMovies(movies || []);
+  // };
   const onClick = async (
     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     e.preventDefault();
     if (!user) return;
     if (exists) {
+      const newArray = favoriteMovies.filter((movie) => movie.id !== id);
+      setFavoriteMovies(newArray);
       await deleteFavoriteMovie(user.email ?? "", id);
     } else {
+      setFavoriteMovies((prev) => [...prev, movie]);
       await addFavoriteMovie(user.email ?? "", movie);
     }
-    await fetchMovies();
+    // await fetchMovies();
   };
   return (
     <button onClick={onClick} className="active:scale-[0.90] duration-200">
