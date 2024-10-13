fd43dba538945be7a4289bc3c1df1fb319aa71a1
dd3dd <jacky81810@gmail.com>
2024-10-13 13:48:53 +0800

    test/auth-status-message

--- __tests__/auth-status-message.test.tsx
+++ __tests__/auth-status-message.test.tsx
@@ -1,63 +1,56 @@
-// AuthStatusMessage.test.tsx
 import { render, screen } from "@testing-library/react";
 import AuthStatusMessage from "@/app/ui/auth/auth-status-message";
 import { useUserContext } from "@/app/user-provider";
 import { vi } from "vitest";
+import { UserContextType } from "@/app/lib/definitions";
 
-
-// 模擬 useUserContext
 vi.mock("@/app/user-provider", () => ({
   useUserContext: vi.fn(),
+}));
+vi.mock("firebase/auth", () => ({
+  getAuth: vi.fn(),
 }));
 
 describe("AuthStatusMessage", () => {
   beforeEach(() => {
-    vi.clearAllMocks(); // 清除所有的 mock 狀態
+    vi.clearAllMocks();
   });
 
   it("renders Spinner when loading", () => {
-    // 模擬 loading 狀態
-    vi.mocked(useUserContext as any).mockReturnValue({ user: null,
-        loading: true})
-        
-    // (useUserContext as any).mockReturnValue({
-    //   user: null,
-    //   loading: true,
-    // });
-
+    vi.mocked(useUserContext).mockReturnValue({
+      user: null,
+      loading: true,
+    } as UserContextType);
     render(<AuthStatusMessage />);
-    
-    // 檢查是否渲染 Spinner
     expect(screen.getByTestId("spinner")).toBeInTheDocument();
   });
 
-//   it("renders Loggedin when user is logged in", () => {
-//     // 模擬已登錄的用戶
-//     const mockUser = { displayName: "John Doe" }; // 這是用戶的模擬數據
-//     (useUserContext as any).mockReturnValue({
-//       user: mockUser,
-//       loading: false,
-//     });
+  it("renders username and routes when user is logged in", () => {
+    const mockUser = { displayName: "dd3dd" };
+    vi.mocked(useUserContext).mockReturnValue({
+      user: mockUser,
+      loading: false,
+    } as UserContextType);
+    render(<AuthStatusMessage />);
 
-//     render(<AuthStatusMessage />);
+    expect(screen.getByText("dd3dd")).toBeInTheDocument();
+    expect(screen.getByText("Top Rated")).toBeInTheDocument();
+    expect(screen.getByText("Search")).toBeInTheDocument();
+    expect(screen.getByText("Watch List")).toBeInTheDocument();
+  });
 
-//     // 檢查是否渲染已登錄狀態
-//     expect(screen.getByText("Hello!")).toBeInTheDocument();
-//     expect(screen.getByText("John Doe")).toBeInTheDocument();
-//     expect(screen.getByText("Continue as Guest")).toBeInTheDocument(); // 確認是否出現其他鏈接選項
-//   });
+  it("renders google login button and Continue as Guest button when user is not logged in", () => {
+    vi.mocked(useUserContext).mockReturnValue({
+      user: null,
+      loading: false,
+    } as UserContextType);
+    render(<AuthStatusMessage />);
 
-//   it("renders NotLoggedin when user is not logged in", () => {
-//     // 模擬未登錄的狀態
-//     (useUserContext as any).mockReturnValue({
-//       user: null,
-//       loading: false,
-//     });
-
-//     render(<AuthStatusMessage />);
-
-//     // 檢查是否渲染未登錄狀態
-//     expect(screen.getByText("or")).toBeInTheDocument();
-//     expect(screen.getByRole("button", { name: /continue as guest/i })).toBeInTheDocument(); // 確認“Continue as Guest”按鈕存在
-//   });
+    expect(screen.getByText("Continue as Guest")).toBeInTheDocument();
+    const button = screen.getByRole("button", {
+      name: /Continue with Google Account/,
+    });
+    expect(button).toBeInTheDocument();
+    expect(button).toBeEnabled();
+  });
 });
